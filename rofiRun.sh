c0a2deaaf44108480487249c8bdb35e5d5395c1a
VerdinTunnel <38482622+verdintunnel@users.noreply.github.com>
2018-10-20 18:23:57 -0700

    added in i3 file

---
+++ rofiRun.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# runs rofi config for i3 file config
+rofi -show run -lines 5 -eh 2 -width 100 -padding 500 -font "System San Francisco Display 18" -color-window "#bf1E201B,#000008,#009933" -color-normal "#e4686e5e,#0066ff,#e4686e5e,#9ba191,#e67300"
+
