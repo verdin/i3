f186b662d570ccf7505fb2b50c2a3069c3af1557
emeremit <emersonp@remitly.com>
2020-03-11 07:59:15 -0700

    set Thunderbird startup window

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 # applications to start at startup
 
-firefox mail.google.com calendar.google.com trello.com
+firefox mail.google.com calendar.google.com trello.com &
 
-slack
+slack &
+
+thunderbird
