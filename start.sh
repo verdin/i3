47efef951788bbfc04bd48fa7619cad8e2764f57
emeremit <emersonp@remitly.com>
2021-01-15 19:02:22 -0800

    added lockscreen

--- start.sh
+++ start.sh
@@ -2,8 +2,12 @@
 
 # applications to start at startup
 
-firefox mail.google.com calendar.google.com trello.com &
+firefox mail.google.com calendar.google.com https://trello.com/b/UF23Y0zU/it-sea-kanban https://range.co/remitly/compose &
 
 slack &
 
-thunderbird
+thunderbird &
+
+chromium https://meet.google.com/ &
+
+update_ohmyzsh
