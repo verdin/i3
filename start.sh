7d46552566429bf3519ccbebdd12301a50c99f2f
emeremit <emersonp@remitly.com>
2022-05-20 10:45:37 -0700

    added display setup commands/scripts

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # applications to start at startup
 
-firefox mail.google.com calendar.google.com https://trello.com/b/UF23Y0zU/it-sea-kanban https://range.co/remitly/compose &
+firefox &
 
 slack &
 
